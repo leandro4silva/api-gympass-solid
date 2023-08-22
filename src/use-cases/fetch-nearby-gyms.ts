cc4ab16c2dce70cd1b622828757ee91f99aae54a
leandro4silva <leandro94.a.s@gmail.com>
2023-08-21 21:24:18 -0300

    feat: (fetch nearby gyms) use case and test

---
+++ src/use-cases/fetch-nearby-gyms.ts
@@ -0,0 +1,29 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface FetchNearbyGymsRequest {
+  userLatitude: number
+  userLongituge: number
+}
+
+interface FetchNearbyGymsResponse {
+  gyms: Gym[]
+}
+
+export class FetchNearbyGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    userLatitude,
+    userLongituge,
+  }: FetchNearbyGymsRequest): Promise<FetchNearbyGymsResponse> {
+    const gyms = await this.gymsRepository.findManyNearby({
+      latitude: userLatitude,
+      longitude: userLongituge,
+    })
+
+    return {
+      gyms,
+    }
+  }
+}
