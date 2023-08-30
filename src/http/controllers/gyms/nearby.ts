7fc8df504fa49e9e3bf75859c3abb3a8eb00511a
leandro4silva <leandro94.a.s@gmail.com>
2023-08-29 21:08:53 -0300

    feat: (controllers) gyms controllers and routes

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query);
+
+  const fetchNearbyUseCase = makeFetchNearbyGymsUseCase();
+
+  const { gyms } = await fetchNearbyUseCase.execute({
+    userLatitude: latitude,
+    userLongituge: longitude,
+  });
+
+  return reply.status(200).send({ gyms });
+}
