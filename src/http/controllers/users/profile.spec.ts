14dd1f24d647542909ed13858e0cbbe307f81886
leandro4silva <leandro94.a.s@gmail.com>
2023-08-30 21:15:44 -0300

    feat: (utils) function to simule auth from tests

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { app } from "@/app";
 import request from "supertest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
 describe("Profile (e2e)", async () => {
   beforeAll(async () => {
@@ -11,18 +12,7 @@ describe("Profile (e2e)", async () => {
   });
 
   it("should be able to get user profile", async () => {
-    await request(app.server).post("/users").send({
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "johndoe@example.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get("/me")
